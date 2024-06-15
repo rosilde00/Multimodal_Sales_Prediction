73d9d998c5c4fad3bb8c35b678607fda482009d8
Rosilde <rosildegaravoglia@gmail.com>
2024-06-15 17:23:46 +0200

    Codici per creare excel dei dati

--- utils/aggregate_sales.py
+++ utils/aggregate_sales.py
@@ -5,11 +5,13 @@ dest_path = 'C:\\ORS\\Data\\aggregated_sales.xlsx'
 data = pd.read_csv(path, names=['locid', 'prodcode', 'colorid', 'year', 'week', 'qty', 'netvalue'], header=None, sep=';')
 data = data.drop(columns=['locid', 'year', 'week', 'netvalue'], axis=1)
 
-for idx in range(0, len(data.values)):
-    ref = data.iloc[idx].values[0]
-    ref = ref[:3] + '_' + ref[4:7] + '_' + ref[7:12] + '_' + ref[12:14] + '_' + data.iloc[idx].values[1] + '_*.jpg'
-    data.iloc[idx,0] = ref
+data_aggregate = data.groupby(['prodcode', 'colorid'], as_index=False).sum()
 
-data = data.drop(columns=['colorid'])  
-data_aggregate = data.groupby('prodcode', as_index=False).sum()
+
+for idx in range(0, len(data_aggregate.values)):
+    ref = data_aggregate.iloc[idx].values[0]
+    ref = ref[:3] + '_' + ref[4:7] + '_' + ref[7:12] + '_' + ref[12:14] + '_' + data_aggregate.iloc[idx].values[1] + '_*.jpg'
+    data_aggregate.iloc[idx,0] = ref
+
+data_aggregate = data_aggregate.drop(columns=['colorid'], axis='columns')
 data_aggregate.to_excel(dest_path, index=False)
---
+++ utils/join_anagrafica_sales.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+def img_ref(season, catr, ccol):
+    new_ref = list()
+    for sn, ca, cc in zip(season, catr, ccol):
+        sn, ca, cc = str(sn), str(ca), str(cc)
+        r = sn + '_' + ca[:3] + '_' + ca[3:8] + '_' + ca[8:] + '_' + cc + '_*.jpg'
+        new_ref.append(r)
+    return new_ref
+
+def join_ref(season, catr):
+    new_ref = list()
+    for sn, ca in zip(season, catr):
+        sn, ca = str(sn), str(ca)
+        r = sn + '-' + ca
+        new_ref.append(r)
+    return new_ref
+
+path_anagrafica = 'C:\\ORS\\Data\\Anagrafica.xlsx' 
+#Stagione, CodiceArticolo, Descrizione, CodiceColore, DescrizioneColore, PianoTaglia, AreaCode,	AreaDescription, CategoryCode
+#CategoryDescription, SectorCode, SectorDescription, DepartmentCode, DepartmentDescription, WaveCode, WaveDescription, AstronomicalSeasonExternalID	
+#AstronomicalSeasonDescription,	SalesSeasonDescription,	SalesSeasonBeginDate, SalesSeasonEndDate
+
+path_sales = 'C:\\ORS\\Data\\sales.csv'
+#LocationId, ProductCode, ColorCode, Year, Week, Quantity, NetValue
+
+path_dest = 'C:\\ORS\\Data\\sales_anagrafica.xlsx'
+
+anagrafica = pd.read_excel(path_anagrafica)
+sales = pd.read_csv(path_sales, names=['LocationId', 'ProductCode', 'CodiceColore', 'Year', 'Week', 'Quantity', 'NetValue'], header=None, sep=';')
+
+ref_images = img_ref(anagrafica['Stagione'], anagrafica['CodiceArticolo'], anagrafica['CodiceColore'])
+ref_join = join_ref(anagrafica['Stagione'], anagrafica['CodiceArticolo'])
+anagrafica = anagrafica.drop(['Stagione', 'CodiceArticolo', 'Descrizione', 'DescrizioneColore', 'AreaDescription', 
+                      'CategoryDescription', 'SectorDescription', 'DepartmentDescription', 'WaveDescription',
+                      'AstronomicalSeasonDescription', 'SalesSeasonBeginDate', 'SalesSeasonEndDate'], axis='columns')
+
+anagrafica['ProductCode'] = ref_join
+anagrafica['IdProdotto'] = ref_images
+
+sales = sales.drop(['NetValue'], axis='columns')
+new_dataset = anagrafica.merge(sales, on=['ProductCode', 'CodiceColore'], how='inner')
+new_dataset = new_dataset.drop(['ProductCode'], axis='columns')
+new_dataset.to_excel(path_dest, index=False)
