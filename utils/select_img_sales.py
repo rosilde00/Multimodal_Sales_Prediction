91c49994d1b25ac4841c9f8d85880d1973c45516
Rosilde <rosildegaravoglia@gmail.com>
2024-06-21 12:02:32 +0200

    Id nelle sales settimanali

--- utils/select_img_sales.py
+++ utils/select_img_sales.py
@@ -1,30 +1,14 @@
-import glob
+import duckdb
 import pandas as pd
-import numpy as np
 
-img_path = 'C:\\ORS\\Data\\Images\\'
+aggregated_path = 'C:\\ORS\\Data\\aggregated_sales_final.xlsx'
 data_path = 'C:\\ORS\\Data\\sales_anagrafica.xlsx'
-dest_path = 'C:\\ORS\\Data\\sales_anagrafica_final.xlsx'
+dest_path = 'C:\\ORS\\Data\\sales_anagrafica_final.csv'
 
 data = pd.read_excel(data_path)
-references = data['IdProdotto'].values
-new_ref = list(references)
-for ref in list(references):
-    images = glob.glob(img_path + ref)
-    if len(images) != 0:
-        images = list(map(lambda img: img[len(img_path):], images))
-        
-        idx = new_ref.index(ref)
-        new_ref.remove(ref)
-        new_ref = new_ref[:idx] + images + new_ref[idx:]
+aggregated = pd.read_excel(aggregated_path)
+aggregated = aggregated.drop(columns=['qty'], axis='columns')
 
-        times = len(images)-1
-        if times != 0:
-            new_data = data.values
-            new_data = np.insert(data.values, idx, [new_data[idx]]*times, axis=0)
-            new_data = pd.DataFrame(new_data)
-            new_data.columns = data.columns
-            data = new_data
-            
-data['IdProdotto'] = new_ref
-data.to_excel(dest_path, index=False)
+res = duckdb.query("SELECT * FROM data JOIN aggregated ON aggregated.prodcode LIKE REPLACE(data.idProdotto, '*', '%')").df()
+res = res.drop(columns=['IdProdotto'], axis='columns')
+res.to_csv(dest_path, index=False)
