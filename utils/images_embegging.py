75c795490bf19caea9838c7b23c1088cf10ecc05
Rosilde <rosildegaravoglia@gmail.com>
2024-10-11 10:17:35 +0200

    modifica embedding immagini

--- utils/images_embegging.py
+++ utils/images_embegging.py
@@ -1,9 +1,10 @@
 import glob
 import os
+from PIL import Image
 from torchvision.models.vision_transformer import vit_b_16
 from torchvision.models.vision_transformer import ViT_B_16_Weights
-from torchvision.io import ImageReadMode
-from torchvision.io import read_image
+from torchvision.models.vision_transformer import vit_l_16
+from torchvision.models.vision_transformer import ViT_L_16_Weights
 from torchvision.transforms import v2
 import torch
 
@@ -13,8 +14,10 @@ IMAGENET_DEFAULT_STD = (0.229, 0.224, 0.225)
 img_path = 'C:\\Users\\GRVRLD00P\\Documents\\Progetto ORS\\Dati\\ResizedImages\\*'
 images = glob.glob(img_path)
 
-vit = vit_b_16(ViT_B_16_Weights.IMAGENET1K_SWAG_E2E_V1)
+vit = vit_l_16(ViT_L_16_Weights.IMAGENET1K_SWAG_E2E_V1)
 transform_img = v2.Compose([
+        v2.Resize((512, 512)),
+        v2.ToImage(),
         v2.ToDtype(torch.float32, scale=True),
         v2.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD)
     ])
@@ -22,7 +25,7 @@ transform_img = v2.Compose([
 embeddings = []
 ids = []
 for path in images:
-    image = transform_img(read_image(path, ImageReadMode.RGB)).unsqueeze(0)
+    image = transform_img(Image.open(path)).unsqueeze(0)
     with torch.no_grad():
         img_emb = vit(image)
     embeddings.append(img_emb.squeeze())
